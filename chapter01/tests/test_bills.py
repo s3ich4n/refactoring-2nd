e993d4af3a1c5a204e9adf2299e91c3206271b1f
s3ich4n <s3ich4n@gmail.com>
2025-01-19 14:16:18 +0900

    test(chatper01): updated html bill test
    - compares between their html structures and contents

--- chapter01/tests/test_bills.py
+++ chapter01/tests/test_bills.py
@@ -2,8 +2,12 @@ import json
 from textwrap import dedent
 
 import pytest
+from bs4 import BeautifulSoup
 
-from chapter01.statements import statement
+from chapter01.statements import (
+    statement,
+    html_statement,
+)
 
 
 @pytest.fixture
@@ -32,3 +36,25 @@ def test_bills(invoices, plays):
 
     # I focused on _the result_ of method. not an format.
     assert statement(invoices, plays).strip() == result
+
+
+def test_html_bills(invoices, plays):
+    expected_html = dedent(
+        """
+        <h1>청구 내역 (고객명: BigCo)</h1>
+        <table>
+        <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+         <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+         <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+         <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+        </table>
+        <p>총액: <em>$1,730.00</em></p>
+        <p>적립 포인트: <em>47</em> credits</p>
+        """
+    ).strip()
+
+    actual_soup = BeautifulSoup(html_statement(invoices, plays), "html.parser")
+    expected_soup = BeautifulSoup(expected_html, "html.parser")
+
+    # compares between their html structures and contents
+    assert str(actual_soup).rstrip() == str(expected_soup).rstrip()
