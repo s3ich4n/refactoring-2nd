37ed716311df0356728abc201f7f04d25f15054a
s3ich4n <s3ich4n@gmail.com>
2025-02-17 01:30:24 +0900

    feat(chapter04): added Province class

---
+++ chapter04/tests/test_province.py
@@ -0,0 +1,16 @@
+from chapter04.src.province import Province
+
+
+def test_sample_province_data():
+    test_data = {
+        "name": "Asia",
+        "producers": [
+            {"name": "Byzantium", "cost": 10, "production": 9},
+            {"name": "Attalia", "cost": 12, "production": 10},
+            {"name": "Sinope", "cost": 10, "production": 6},
+        ],
+        "demand": 30,
+        "price": 20,
+    }
+
+    assert Province(**test_data)
