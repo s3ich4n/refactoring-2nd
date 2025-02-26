e33bec56f52d2539c674ece9ae442b8ff9351554
s3ich4n <s3ich4n@gmail.com>
2025-02-27 01:21:07 +0900

    refactor(chapter06): implement DefaultOwnerManager for owner data encapsulation

--- chapter06/tests/case06/test_case06.py
+++ chapter06/tests/case06/test_case06.py
@@ -1,17 +1,18 @@
 from chapter06.src.case06.case06 import (
-    default_owner,
     Spaceship,
+    manager,
 )
 
 
 def test_access_data():
-    space_ship = Spaceship(owner=default_owner)
+    space_ship = Spaceship(owner=manager.owner)
 
-    assert space_ship.owner == default_owner
+    assert space_ship.owner["first_name"] == "Martin"
+    assert space_ship.owner["last_name"] == "Fowler"
 
 
 def test_modify_data():
-    default_owner = {"first_name": "Rebecca", "last_name": "Parsons"}
+    manager.owner = {"first_name": "Rebecca", "last_name": "Parsons"}
 
-    assert default_owner["first_name"] == "Rebecca"
-    assert default_owner["last_name"] == "Parsons"
+    assert manager.owner["first_name"] == "Rebecca"
+    assert manager.owner["last_name"] == "Parsons"
