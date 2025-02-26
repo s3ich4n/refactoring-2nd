f77851d5bc054bf3ef16ed9e96352b9ce0df7ff5
s3ich4n <s3ich4n@gmail.com>
2025-02-27 00:35:07 +0900

    refactor(chapter06): add is_in_new_england function to improve state code validation

--- chapter06/src/case05/case_05_2.py
+++ chapter06/src/case05/case_05_2.py
@@ -13,4 +13,8 @@ class Customer:
 
 def in_new_england(a_customer):
     state_code = a_customer.address.state
+    return is_in_new_england(state_code)
+
+
+def is_in_new_england(state_code):
     return state_code in ["MA", "CT", "ME", "VT", "NH", "RI"]
