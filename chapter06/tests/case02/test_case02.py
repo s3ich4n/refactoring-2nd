bbad8e69e7582465b1a50d21423d0276f14269e8
s3ich4n <s3ich4n@gmail.com>
2025-02-26 19:03:54 +0900

    refactor(chapter06): simplify rating function and enhance test coverage

--- chapter06/tests/case02/test_case02.py
+++ chapter06/tests/case02/test_case02.py
@@ -11,5 +11,12 @@ def a_driver():
     return Driver(number_of_late_deliveries=6)
 
 
-def test_case02(a_driver):
-    assert rating(a_driver) == 2
+@pytest.mark.parametrize(
+    "a_driver,expected",
+    [
+        (Driver(number_of_late_deliveries=6), 2),
+        (Driver(number_of_late_deliveries=5), 1),
+    ]
+)
+def test_case02(a_driver, expected):
+    assert rating(a_driver) == expected
