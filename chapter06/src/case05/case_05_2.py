dd9fd1fe6dff3cc5b1e607a678d664092e402a3b
s3ich4n <s3ich4n@gmail.com>
2025-02-27 00:38:45 +0900

    refactor(chapter06): update in_new_england function to accept state code directly

--- chapter06/src/case05/case_05_2.py
+++ chapter06/src/case05/case_05_2.py
@@ -11,9 +11,5 @@ class Customer:
     address: Address
 
 
-def in_new_england(a_customer):
-    return is_in_new_england(a_customer.address.state)
-
-
-def is_in_new_england(state_code):
+def in_new_england(state_code):
     return state_code in ["MA", "CT", "ME", "VT", "NH", "RI"]
