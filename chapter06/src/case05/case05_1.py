88ce64a7118168722d1b4e593fcfa46d37a464e4
s3ich4n <s3ich4n@gmail.com>
2025-02-27 00:20:45 +0900

    refactor(case05): add priority support for reservations

--- chapter06/src/case05/case05_1.py
+++ chapter06/src/case05/case05_1.py
@@ -13,4 +13,10 @@ class Book:
         self._reservations = []
 
     def add_reservation(self, customer):
+        self.do_add_reservation(customer, False)
+
+    def do_add_reservation(self, customer, is_priority):
+        assert (
+            is_priority is True or is_priority is False
+        )  # 이러면 assertion으로 체크 가능
         self._reservations.append(customer)
