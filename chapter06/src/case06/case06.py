066496c63b82dff4bcd3a394482076d005eccdd9
s3ich4n <s3ich4n@gmail.com>
2025-02-27 01:31:43 +0900

    feat(chapter06): encapsulate owner data using Owner class and update access methods

--- chapter06/src/case06/case06.py
+++ chapter06/src/case06/case06.py
@@ -1,22 +1,29 @@
 from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
+class Owner:
+    first_name: str
+    last_name: str
+
+
 @dataclass
 class Spaceship:
-    owner: dict
+    owner: Owner
 
 
-class DefaultOwnerManager:
-    def __init__(self):
-        self._owner = {"first_name": "Martin", "last_name": "Fowler"}
-
-    @property
-    def owner(self):
-        return self._owner.copy()
-
-    @owner.setter
-    def owner(self, new_owner):
-        self._owner = new_owner
+# 모듈 레벨 변수
+_default_owner = Owner(first_name="Martin", last_name="Fowler")
 
 
-manager = DefaultOwnerManager()
+# getter 함수 - 이미 불변 객체이므로 그대로 반환해도 안전
+def get_owner():
+    """현재 소유자 정보 반환"""
+    return _default_owner
+
+
+# setter 함수
+def set_owner(new_owner):
+    """소유자 정보 변경"""
+    global _default_owner
+    _default_owner = new_owner
