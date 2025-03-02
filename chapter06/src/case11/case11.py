4660e8821003a3a8d2a5a77ef2161bd695c0e450
s3ich4n <s3ich4n@gmail.com>
2025-03-03 03:24:03 +0900

    refactor(chapter6): enhance apply_shipping function to utilize quantity and discount from price_data

--- chapter06/src/case11/case11.py
+++ chapter06/src/case11/case11.py
@@ -6,19 +6,19 @@ def price_order(product, quantity, shipping_method):
         * product["discountRate"]
     )
 
-    price_data = {"base_price": base_price}
+    price_data = {"base_price": base_price, "quantity": quantity, "discount": discount}
 
-    price = apply_shipping(price_data, shipping_method, quantity, discount)
+    price = apply_shipping(price_data, shipping_method)
 
     return price
 
 
-def apply_shipping(price_data, shipping_method, quantity, discount):
+def apply_shipping(price_data, shipping_method):
     if price_data["base_price"] > shipping_method["discountThreshold"]:
         shipping_per_case = shipping_method["discountedFee"]
     else:
         shipping_per_case = shipping_method["feePerCase"]
-    shipping_cost = quantity * shipping_per_case
-    price = price_data["base_price"] - discount + shipping_cost
+    shipping_cost = price_data["quantity"] * shipping_per_case
+    price = price_data["base_price"] - price_data["discount"] + shipping_cost
 
     return price
