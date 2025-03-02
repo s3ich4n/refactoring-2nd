53253baf13d95f1f399af6304b2c14e9c6a7e13d
s3ich4n <s3ich4n@gmail.com>
2025-03-03 03:17:52 +0900

    refactor(chapter6): update apply_shipping function to use price_data for base price

--- chapter06/src/case11/case11.py
+++ chapter06/src/case11/case11.py
@@ -6,19 +6,19 @@ def price_order(product, quantity, shipping_method):
         * product["discountRate"]
     )
 
-    price_data = {}
+    price_data = {"base_price": base_price}
 
-    price = apply_shipping(base_price, shipping_method, quantity, discount)
+    price = apply_shipping(price_data, shipping_method, quantity, discount)
 
     return price
 
 
-def apply_shipping(price_data, base_price, shipping_method, quantity, discount):
-    if base_price > shipping_method["discountThreshold"]:
+def apply_shipping(price_data, shipping_method, quantity, discount):
+    if price_data["base_price"] > shipping_method["discountThreshold"]:
         shipping_per_case = shipping_method["discountedFee"]
     else:
         shipping_per_case = shipping_method["feePerCase"]
     shipping_cost = quantity * shipping_per_case
-    price = base_price - discount + shipping_cost
+    price = price_data["base_price"] - discount + shipping_cost
 
     return price
