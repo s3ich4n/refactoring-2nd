4c75355f6356ca123afc58e5332bc2445b1d2af2
s3ich4n <s3ich4n@gmail.com>
2025-03-03 02:27:13 +0900

    refactor(chapter6): modify apply_shipping function to accept price_data parameter

--- chapter06/src/case11/case11.py
+++ chapter06/src/case11/case11.py
@@ -6,12 +6,14 @@ def price_order(product, quantity, shipping_method):
         * product["discountRate"]
     )
 
+    price_data = {}
+
     price = apply_shipping(base_price, shipping_method, quantity, discount)
 
     return price
 
 
-def apply_shipping(base_price, shipping_method, quantity, discount):
+def apply_shipping(price_data, base_price, shipping_method, quantity, discount):
     if base_price > shipping_method["discountThreshold"]:
         shipping_per_case = shipping_method["discountedFee"]
     else:
