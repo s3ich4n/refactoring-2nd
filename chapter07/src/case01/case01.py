d9f0aed76d8ef866453f1d06a349d326423300c7
s3ich4n <s3ich4n@gmail.com>
2025-03-03 22:33:14 +0900

    refactor(chapter7): encapsulate organization data in a class and update data retrieval methods

--- chapter07/src/case01/case01.py
+++ chapter07/src/case01/case01.py
@@ -1,11 +1,18 @@
-organization = {"name": "Acme Gooseberries", "country": "GB"}
+class Organization:
+    def __init__(self, data):
+        self.data = data
 
 
 # 임시로 이름 붙인 것
 def get_raw_data_of_organization():
+    return organization.data
+
+
+def get_organization():
     return organization
 
 
 # client 1
-result = f"<h1>{get_raw_data_of_organization()['name']}</h1>"
-get_raw_data_of_organization()["name"] = "new name"
+organization = Organization({"name": "Acme Gooseberries", "country": "GB"})
+result = f"<h1>{get_organization().data['name']}</h1>"
+get_organization().data["name"] = "new name"
