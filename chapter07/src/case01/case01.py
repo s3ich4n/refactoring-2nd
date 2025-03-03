fb38e282e65613245fb88f6c172887d570fc4003
s3ich4n <s3ich4n@gmail.com>
2025-03-03 22:23:15 +0900

    refactor(chapter7): encapsulate organization data retrieval in a function

--- chapter07/src/case01/case01.py
+++ chapter07/src/case01/case01.py
@@ -1,6 +1,11 @@
 organization = {"name": "Acme Gooseberries", "country": "GB"}
 
 
+# 임시로 이름 붙인 것
+def get_raw_data_of_organization():
+    return organization
+
+
 # client 1
-result = f"<h1>{organization['name']}</h1>"
-organization["name"] = "new name"
+result = f"<h1>{get_raw_data_of_organization()['name']}</h1>"
+get_raw_data_of_organization()["name"] = "new name"
