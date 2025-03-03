df472392577681ba3f52f7d5e8ceabea91c97335
s3ich4n <s3ich4n@gmail.com>
2025-03-03 22:38:09 +0900

    refactor(chapter7): improve organization class by adding attributes and removing unnecessary functions

--- chapter07/src/case01/case01.py
+++ chapter07/src/case01/case01.py
@@ -1,18 +1,10 @@
 class Organization:
     def __init__(self, data):
-        self.data = data
-
-
-# 임시로 이름 붙인 것
-def get_raw_data_of_organization():
-    return organization.data
-
-
-def get_organization():
-    return organization
+        self.name = data["name"]
+        self.country = data["country"]
 
 
 # client 1
 organization = Organization({"name": "Acme Gooseberries", "country": "GB"})
-result = f"<h1>{get_organization().data['name']}</h1>"
-get_organization().data["name"] = "new name"
+result = f"<h1>{organization.name}</h1>"
+organization.name = "new name"
