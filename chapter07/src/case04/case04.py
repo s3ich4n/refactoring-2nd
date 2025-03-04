966d400fc47c5682c52d508aa4403fc57d48999e
s3ich4n <s3ich4n@gmail.com>
2025-03-05 01:36:36 +0900

    refactor(chapter7): separate discount factor calculation from price property

--- chapter07/src/case04/case04.py
+++ chapter07/src/case04/case04.py
@@ -8,11 +8,16 @@ class Order:
         return self._quantity * self._item.price
 
     @property
-    def price(self):
+    def discount_factor(self):
         discount_factor = 0.98
         if self.base_price > 1000:
             discount_factor -= 0.03
-        return self.base_price * discount_factor
+
+        return discount_factor
+
+    @property
+    def price(self):
+        return self.base_price * self.discount_factor
 
 
 class Item:
