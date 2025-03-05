47a877c5a17c3adc7cd2ef29c3a2bce46915b7d4
s3ich4n <s3ich4n@gmail.com>
2025-03-06 00:50:43 +0900

    refactor(chapter7): replace TrackingInformation with Shipment class and update tests

--- chapter07/src/case06/case06.py
+++ chapter07/src/case06/case06.py
@@ -1,10 +1,23 @@
-# tracking.py
-
-
-class TrackingInformation:
+class Shipment:
     def __init__(self):
+        self._tracking_information = None
         self._shipping_company = None
         self._tracking_number = None
+
+    @property
+    def tracking_info(self):
+        # 자바스크립트의 get trackingInfo() { return this._trackingInformation.display; }에 해당
+        return (
+            self._tracking_information.display if self._tracking_information else None
+        )
+
+    @property
+    def tracking_information(self):
+        return self._tracking_information
+
+    @tracking_information.setter
+    def tracking_information(self, a_tracking_information):
+        self._tracking_information = a_tracking_information
 
     @property
     def shipping_company(self):
@@ -22,26 +35,5 @@ class TrackingInformation:
     def tracking_number(self, arg):
         self._tracking_number = arg
 
-    @property
     def display(self):
         return f"{self.shipping_company}: {self.tracking_number}"
-
-
-class Shipment:
-    def __init__(self):
-        self._tracking_information = None
-
-    @property
-    def tracking_info(self):
-        # 자바스크립트의 get trackingInfo() { return this._trackingInformation.display; }에 해당
-        return (
-            self._tracking_information.display if self._tracking_information else None
-        )
-
-    @property
-    def tracking_information(self):
-        return self._tracking_information
-
-    @tracking_information.setter
-    def tracking_information(self, a_tracking_information):
-        self._tracking_information = a_tracking_information
--- chapter07/tests/case06/test_case06.py
+++ chapter07/tests/case06/test_case06.py
@@ -1,31 +1,8 @@
 # tests/test_tracking.py
 import pytest
 from src.case06.case06 import (
-    TrackingInformation,
     Shipment,
 )
-
-
-def test_tracking_information_basic():
-    """
-    TrackingInformation을 생성하고,
-    shippingCompany와 trackingNumber를 세터로 설정한 뒤
-    제대로 게터로 반환되는지, display 문자열이 정상인지 검증
-    """
-    tracking = TrackingInformation()
-
-    # 초기 상태 확인 (None)
-    assert tracking.shipping_company is None
-    assert tracking.tracking_number is None
-
-    # 세터로 값 설정
-    tracking.shipping_company = "FedEx"
-    tracking.tracking_number = "123456"
-
-    # 게터 및 display 확인
-    assert tracking.shipping_company == "FedEx"
-    assert tracking.tracking_number == "123456"
-    assert tracking.display == "FedEx: 123456"
 
 
 @pytest.mark.parametrize(
@@ -40,31 +17,27 @@ def test_tracking_information_param(company, number, expected_display):
     """
     여러 케이스를 파라미터라이즈하여 TrackingInformation 테스트
     """
-    tracking = TrackingInformation()
-    tracking.shipping_company = company
-    tracking.tracking_number = number
+    shipment = Shipment()
+    shipment.shipping_company = company
+    shipment.tracking_number = number
 
-    assert tracking.display == expected_display
+    assert shipment.display() == expected_display
 
 
 def test_shipment_basic():
     """
-    Shipment에 TrackingInformation을 연결해보고,
-    trackingInfo 프로퍼티가 올바른 문자열을 반환하는지 확인
+    Shipment의 요소가 제대로 포함되어있나 확인
     """
     shipment = Shipment()
 
     # 기본적으로 None인 경우 확인
     assert shipment.tracking_info is None
 
-    # TrackingInformation 생성 및 설정
-    tracking = TrackingInformation()
-    tracking.shipping_company = "FedEx"
-    tracking.tracking_number = "654321"
+    shipment.shipping_company = "FedEx"
+    shipment.tracking_number = "654321"
 
     # Shipment에 세팅
-    shipment.tracking_information = tracking
-    assert shipment.tracking_info == "FedEx: 654321"
+    assert shipment.display() == "FedEx: 654321"
 
 
 @pytest.mark.parametrize(
@@ -80,11 +53,8 @@ def test_shipment_param(company, number, expected_info):
     다양한 입력값에 대해 trackingInfo가 올바른지 테스트
     """
     shipment = Shipment()
-    tracking = TrackingInformation()
 
-    tracking.shipping_company = company
-    tracking.tracking_number = number
+    shipment.shipping_company = company
+    shipment.tracking_number = number
 
-    shipment.tracking_information = tracking
-
-    assert shipment.tracking_info == expected_info
+    assert shipment.display() == expected_info
