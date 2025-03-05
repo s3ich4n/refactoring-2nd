07ebffb8c26d91622dc93de7ecb16c7b8fe1e5e9
s3ich4n <s3ich4n@gmail.com>
2025-03-05 23:37:45 +0900

    feat(chapter7): refactor Person class to use TelephoneNumber for office contact

--- chapter07/src/case05/case05.py
+++ chapter07/src/case05/case05.py
@@ -1,7 +1,6 @@
 class Person:
-    def __init__(self, office_area_code, office_number):
-        self._office_area_code = office_area_code
-        self._office_number = office_number
+    def __init__(self, telephone_number):
+        self._telephone_number = telephone_number
 
     @property
     def office_area_code(self):
@@ -10,3 +9,12 @@ class Person:
     @property
     def office_number(self):
         return self._office_number
+
+
+class TelephoneNumber:
+    def __init__(self, area_code):
+        self._area_code = area_code
+
+    @property
+    def area_code(self):
+        return self._area_code
