6fe5561d165d57cfffe64b9c44c7d9a416349c5a
s3ich4n <s3ich4n@gmail.com>
2025-03-06 01:06:17 +0900

    feat(chapter7): add Person and Department classes with basic functionality and tests

---
+++ chapter07/src/case07/case07.py
@@ -0,0 +1,41 @@
+# organization.py
+
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+        self._department = None  # 기본값
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def department(self):
+        return self._department
+
+    @department.setter
+    def department(self, arg):
+        self._department = arg
+
+
+class Department:
+    def __init__(self):
+        self._charge_code = None
+        self._manager = None
+
+    @property
+    def charge_code(self):
+        return self._charge_code
+
+    @charge_code.setter
+    def charge_code(self, arg):
+        self._charge_code = arg
+
+    @property
+    def manager(self):
+        return self._manager
+
+    @manager.setter
+    def manager(self, arg):
+        self._manager = arg
