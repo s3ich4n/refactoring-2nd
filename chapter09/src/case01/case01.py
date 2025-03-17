7ad5c8c1a9ef12050155fc7097d0bc40cf5026dc
s3ich4n <s3ich4n@gmail.com>
2025-03-17 23:07:35 +0900

    refactor(chapter09): distance calculation into primary and secondary functions

--- chapter09/src/case01/case01.py
+++ chapter09/src/case01/case01.py
@@ -1,18 +1,28 @@
 def distance_travelled(scenario, time):
-    result = 0
-    primary_acceleration = scenario.primary_force / scenario.mass
-    primary_time = min(time, scenario.delay)
-    result = 0.5 * primary_acceleration * primary_time * primary_time
+    result = calculate_primary_distance(scenario, time)
 
-    secondary_time = time - scenario.delay
-    if secondary_time > 0:
-        primary_velocity = primary_acceleration * scenario.delay
-        secondary_acceleration = (
-            scenario.primary_force + scenario.secondary_force
-        ) / scenario.mass  # 두번째 대입 전까지
-        result += (
-            primary_velocity * secondary_time
-            + 0.5 * secondary_acceleration * secondary_time * secondary_time
-        )
+    if time > scenario.delay:
+        result += calculate_secondary_distance(scenario, time)
 
     return result
+
+
+def calculate_primary_distance(scenario, primary_time):
+    primary_time = min(primary_time, scenario.delay)
+    primary_acceleration = scenario.primary_force / scenario.mass
+    return 0.5 * primary_acceleration * primary_time * primary_time
+
+
+def calculate_secondary_distance(scenario, time):
+    secondary_time = time - scenario.delay
+    primary_acceleration = scenario.primary_force / scenario.mass
+    primary_velocity = primary_acceleration * scenario.delay
+
+    secondary_acceleration = (
+        scenario.primary_force + scenario.secondary_force
+    ) / scenario.mass
+
+    return (
+        primary_velocity * secondary_time
+        + 0.5 * secondary_acceleration * secondary_time * secondary_time
+    )
