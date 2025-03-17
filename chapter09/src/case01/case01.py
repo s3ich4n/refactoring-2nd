96669839c38f40348f740718b6a6ddd210c6aaf4
s3ich4n <s3ich4n@gmail.com>
2025-03-17 23:02:57 +0900

    refactor(chapter09): improve variable naming for clarity in distance calculation

--- chapter09/src/case01/case01.py
+++ chapter09/src/case01/case01.py
@@ -1,16 +1,16 @@
 def distance_travelled(scenario, time):
     result = 0
-    acc = scenario.primary_force / scenario.mass
+    primary_acceleration = scenario.primary_force / scenario.mass
     primary_time = min(time, scenario.delay)
-    result = 0.5 * acc * primary_time * primary_time
+    result = 0.5 * primary_acceleration * primary_time * primary_time
 
     secondary_time = time - scenario.delay
     if secondary_time > 0:
-        primary_velocity = acc * scenario.delay
-        acc = (scenario.primary_force + scenario.secondary_force) / scenario.mass
+        primary_velocity = primary_acceleration * scenario.delay
+        acc = (scenario.primary_force + scenario.secondary_force) / scenario.mass   # 두번째 대입 전까지
         result += (
-                primary_velocity * secondary_time
-                + 0.5 * acc * secondary_time * secondary_time
+            primary_velocity * secondary_time
+            + 0.5 * acc * secondary_time * secondary_time
         )
 
     return result
