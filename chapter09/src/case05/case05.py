7135c0b8473fb0d790038a711e8cf98d5c211f34
s3ich4n <s3ich4n@gmail.com>
2025-03-18 01:34:32 +0900

    feat(chapter05): add CustomerRepository class and legacy singleton functions for customer management

--- chapter09/src/case05/case05.py
+++ chapter09/src/case05/case05.py
@@ -15,3 +15,33 @@ class Order:
     @property
     def customer(self):
         return self._customer
+
+
+class CustomerRepository:
+    def __init__(self):
+        self._customers = {}
+
+    def register_customer(self, id):
+        if id not in self._customers:
+            self._customers[id] = Customer(id)
+        return self.find_customer(id)
+
+    def find_customer(self, id):
+        return self._customers.get(id)
+
+
+# 기존 API와의 호환성을 위한 싱글톤 인스턴스
+_default_repository = None
+
+
+def initialize():
+    global _default_repository
+    _default_repository = CustomerRepository()
+
+
+def register_customer(id):
+    return _default_repository.register_customer(id)
+
+
+def find_customer(id):
+    return _default_repository.find_customer(id)
