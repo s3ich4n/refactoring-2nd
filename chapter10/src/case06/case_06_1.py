a235ea3a8d08478592cb7a5eb8851131048987e3
s3ich4n <s3ich4n@gmail.com>
2025-04-02 12:49:53 +0900

    feat(chapter10): enhance discount application logic and add villain search functionality with tests

--- chapter10/src/case06/case_06_1.py
+++ chapter10/src/case06/case_06_1.py
@@ -3,8 +3,12 @@ class Customer:
         self.discount_rate = discount_rate
 
     def apply_discount(self, a_number):
-        return (
-            a_number - (self.discount_rate * a_number)
-            if self.discount_rate
-            else a_number
-        )
+        assert a_number >= 0, "금액은 음수가 될 수 없습니다"
+
+        if self.discount_rate:
+            assert isinstance(
+                self.discount_rate, (int, float)
+            ), "할인율은 숫자여야 합니다"
+            return a_number - (self.discount_rate * a_number)
+        else:
+            return a_number
