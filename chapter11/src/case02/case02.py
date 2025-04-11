c77214e22a73e6af1c7eb950b6167bd06ccdf00f
s3ich4n <s3ich4n@gmail.com>
2025-04-12 02:41:16 +0900

    refactor(chapter11): replace top_band with within_band for upper limit calculation

--- chapter11/src/case02/case02.py
+++ chapter11/src/case02/case02.py
@@ -9,18 +9,10 @@ def base_charge(usage):
     amount = (
         within_band(usage, 0, 100) * 0.03
         + within_band(usage, 100, 200) * 0.05
-        + top_band(usage) * 0.03
+        + within_band(usage, 200, float("inf")) * 0.07
     )
 
     return usd(amount)
-
-
-def middle_band(usage):
-    return min(usage, 200) - 100 if usage > 100 else 0
-
-
-def top_band(usage):
-    return 200 if usage > 200 else 0
 
 
 def within_band(usage, bottom, top):
