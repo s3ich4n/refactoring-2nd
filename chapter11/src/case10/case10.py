621cb59661fff0b776d711f2158ded44d8d9f15b
s3ich4n <s3ich4n@gmail.com>
2025-04-13 01:47:59 +0900

    feat(chapter11): add ChargeCalculator class for calculating charges based on customer usage and provider fees

---
+++ chapter11/src/case10/case10.py
@@ -0,0 +1,37 @@
+class ChargeCalculator:
+    """
+    요금 계산을 위한 클래스
+    """
+    
+    def __init__(self, customer, usage, provider):
+        """
+        ChargeCalculator 클래스 초기화
+        
+        Args:
+            customer: 고객 정보
+            usage: 사용량
+            provider: 서비스 제공자
+        """
+        self._customer = customer
+        self._usage = usage
+        self._provider = provider
+    
+    @property
+    def base_charge(self):
+        """
+        기본 요금 계산
+        
+        Returns:
+            float: 계산된 기본 요금
+        """
+        return self._customer.base_rate * self._usage
+    
+    @property
+    def charge(self):
+        """
+        총 요금 계산
+        
+        Returns:
+            float: 계산된 총 요금
+        """
+        return self.base_charge + self._provider.connection_charge
