8d9bf77dee55f16514e5a83577248b28d4cd49e8
s3ich4n <s3ich4n@gmail.com>
2025-04-12 19:46:53 +0900

    feat(chapter11): rename neo_within_range to zz_neo_within_range and update references

--- chapter11/src/case04/case04_1.py
+++ chapter11/src/case04/case04_1.py
@@ -19,7 +19,8 @@ class HeatingPlan:
     def __init__(self, temperature_range):
         self.temperature_range = temperature_range
 
-    def neo_within_range(self, a_number_range: TempRange): ...
+    def zz_neo_within_range(self, a_number_range: TempRange):
+        return self.within_range(a_number_range.low, a_number_range.high)
 
     def within_range(self, bottom, top):
         """주어진 범위가 계획의 온도 범위 내에 있는지 확인"""
@@ -32,5 +33,5 @@ def check_room_temperature(room, plan, alerts):
     """방 온도가 계획의 범위를 벗어났는지 확인하고 경고를 추가"""
     low = room.days_temp_range.low
     high = room.days_temp_range.high
-    if not plan.within_range(low, high):
+    if not plan.zz_neo_within_range(room.days_temp_range):
         alerts.append("room temperature went outside range")
