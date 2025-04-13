8854f94dd89a9fbb5e54ced1c33291de6efff793
s3ich4n <s3ich4n@gmail.com>
2025-04-13 22:23:25 +0900

    refactor(chapter12): update Party class initialization in Employee and Department subclasses

--- chapter12/src/case03/case03.py
+++ chapter12/src/case03/case03.py
@@ -1,16 +1,16 @@
-class Party: ...
+class Party:
+    def __init__(self, name):
+        self._name = name
 
 
 class Employee(Party):
     def __init__(self, name, user_id, monthly_cost):
-        super().__init__()
-        self._name = name
+        super().__init__(name)
         self._user_id = user_id
         self._monthly_cost = monthly_cost
 
 
 class Department(Party):
     def __init__(self, name, staff):
-        super().__init__()
-        self._name = name
+        super().__init__(name)
         self._staff = staff
