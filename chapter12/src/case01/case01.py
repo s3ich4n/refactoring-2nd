3c269a38098240314e6e46c82475004276684cb9
s3ich4n <s3ich4n@gmail.com>
2025-04-13 21:32:12 +0900

    refactor(chapter12): consolidate annual_cost method in Party class

--- chapter12/src/case01/case01.py
+++ chapter12/src/case01/case01.py
@@ -2,18 +2,15 @@ class Party:
     def __init__(self):
         self.monthly_cost = 10
 
+    def annual_cost(self):
+        return self.monthly_cost * 12
+
 
 class Department(Party):
     def __init__(self):
         super().__init__()
 
-    def annual_cost(self):
-        return self.monthly_cost * 12
-
 
 class Employee(Party):
     def __init__(self):
         super().__init__()
-
-    def annual_cost(self):
-        return self.monthly_cost * 12
