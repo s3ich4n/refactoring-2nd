4e93da074177ce31926881d741ea3efcd64b30f4
s3ich4n <s3ich4n@gmail.com>
2025-04-13 14:29:38 +0900

    refactor(chapter11): initialize total_ascent within calculate_ascent function

--- chapter11/src/case11/case11.py
+++ chapter11/src/case11/case11.py
@@ -1,13 +1,11 @@
 from collections import namedtuple
 
-total_ascent = 0
-
 # 테스트를 위한 간단한 Point 클래스
 Point = namedtuple("Point", ["elevation"])
 
 
 def calculate_ascent(points):
-    global total_ascent
+    total_ascent = 0
     for i in range(1, len(points)):
         vertical_change = points[i].elevation - points[i - 1].elevation
         total_ascent += vertical_change if vertical_change > 0 else 0
