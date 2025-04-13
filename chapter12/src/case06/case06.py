46da2639b4926403520091d5bd0f60fe8be143d9
s3ich4n <s3ich4n@gmail.com>
2025-04-14 00:15:55 +0900

    refactor(chapter12): simplify Employee constructor and update string representation

--- chapter12/src/case06/case06.py
+++ chapter12/src/case06/case06.py
@@ -1,19 +1,18 @@
 class Employee:
-    def __init__(self, name, employee_type):
+    def __init__(self, name):
         self._name = name
-        self._employee_type = employee_type
 
     def __str__(self):
-        return f"{self._name} ({self._employee_type})"
+        return f"{self.__class__.__name__} - {self._name}"
 
     @classmethod
     def create(cls, name, employee_type):
         if employee_type == "engineer":
-            return Engineer(name, "engineer")
+            return Engineer(name)
         elif employee_type == "manager":
-            return Manager(name, "manager")
+            return Manager(name)
         elif employee_type == "salesperson":
-            return Salesperson(name, "salesman")
+            return Salesperson(name)
         else:
             raise NotImplementedError(f"Employee cannot be of type {employee_type}")
 
