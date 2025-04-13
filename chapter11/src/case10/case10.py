3ba5e7976f105aec184a552c404b0bf9a1b4d08d
s3ich4n <s3ich4n@gmail.com>
2025-04-13 13:15:59 +0900

    refactor(chapter11): remove customer parameter from ChargeCalculator and adjust base charge calculation

--- chapter11/src/case10/case10.py
+++ chapter11/src/case10/case10.py
@@ -1,18 +1,21 @@
+class Customer:
+    def __init__(self, base_rate):
+        self.base_rate = base_rate
+
+
 class ChargeCalculator:
     """
     요금 계산을 위한 클래스
     """
 
-    def __init__(self, customer, usage, provider):
+    def __init__(self, usage, provider):
         """
         ChargeCalculator 클래스 초기화
 
         Args:
-            customer: 고객 정보
             usage: 사용량
             provider: 서비스 제공자
         """
-        self._customer = customer
         self._usage = usage
         self._provider = provider
 
@@ -20,8 +23,11 @@ class ChargeCalculator:
         """
         총 요금 계산
 
+        Args:
+            customer: 고객 정보
+
         Returns:
             float: 계산된 총 요금
         """
-        base_charge = self._customer.base_rate * self._usage
+        base_charge = customer.base_rate * self._usage
         return base_charge + self._provider.connection_charge
