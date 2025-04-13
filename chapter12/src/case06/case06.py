dae7c82ef9024de1d93ba850df4d3391a8af0306
s3ich4n <s3ich4n@gmail.com>
2025-04-14 00:09:11 +0900

    refactor(chapter12): remove employee type validation from Employee constructor

--- chapter12/src/case06/case06.py
+++ chapter12/src/case06/case06.py
@@ -1,12 +1,7 @@
 class Employee:
     def __init__(self, name, employee_type):
-        self.validate_type(employee_type)
         self._name = name
         self._employee_type = employee_type
-
-    def validate_type(self, arg):
-        if arg not in ["engineer", "manager", "salesman"]:
-            raise ValueError(f"Employee cannot be of type {arg}")
 
     def __str__(self):
         return f"{self._name} ({self._employee_type})"
