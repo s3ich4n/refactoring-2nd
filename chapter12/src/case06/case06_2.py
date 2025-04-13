8d3d38ddbbc7125874c9e953c43c2d0d816ad8ce
s3ich4n <s3ich4n@gmail.com>
2025-04-14 00:38:59 +0900

    feat(chapter12): enhance Employee class with dynamic type creation and string representation

--- chapter12/src/case06/case06_2.py
+++ chapter12/src/case06/case06_2.py
@@ -9,16 +9,54 @@ class Employee:
             raise ValueError(f"Employee cannot be of type {arg}")
 
     @property
+    def type_string(self):
+        return str(self._type)
+
+    @property
     def type(self):
         return self._type
 
     @type.setter
     def type(self, arg):
-        self._type = arg
+        self._type = Employee.create_employee_type(arg)
 
     @property
     def capitalized_type(self):
-        return self._type.capitalize()
+        return self.type_string.capitalize()
+
+    @classmethod
+    def create_employee_type(cls, employee_type):
+        if employee_type == "engineer":
+            return Engineer()
+        elif employee_type == "manager":
+            return Manager()
+        elif employee_type == "salesperson":
+            return Salesperson()
+        else:
+            raise NotImplementedError(f"Employee cannot be of type {employee_type}")
 
     def __str__(self):
         return f"{self._name} ({self.capitalized_type})"
+
+
+class EmployeeType:
+    def __init__(self, value):
+        self._value = value
+
+    def __str__(self):
+        return self._value
+
+
+class Engineer(EmployeeType):
+    def __str__(self):
+        return "engineer"
+
+
+class Manager(EmployeeType):
+    def __str__(self):
+        return "manager"
+
+
+class Salesperson(EmployeeType):
+    def __str__(self):
+        return "salesperson"
