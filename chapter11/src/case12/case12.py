65bc8b39da6fcd9c011be48e271a17d0f94bfa4d
s3ich4n <s3ich4n@gmail.com>
2025-04-13 15:29:13 +0900

    feat(chapter11): add ShippingRules and CountryData classes with local_shipping_rules function

---
+++ chapter11/src/case12/case12.py
@@ -0,0 +1,37 @@
+class ShippingRules:
+    """배송 규칙을 나타내는 클래스"""
+
+    def __init__(self, data):
+        """
+        배송 규칙 초기화
+
+        Args:
+            data: 배송 규칙 데이터
+        """
+        self.data = data
+
+
+class CountryData:
+    """국가 데이터를 관리하는 클래스"""
+
+    def __init__(self):
+        """국가 데이터 초기화"""
+        self.shipping_rules = {}
+
+
+def local_shipping_rules(country, country_data):
+    """
+    특정 국가의 배송 규칙을 조회하는 함수
+
+    Args:
+        country (str): 국가 코드
+        country_data (CountryData): 국가 데이터 객체
+
+    Returns:
+        ShippingRules: 해당 국가의 배송 규칙
+    """
+    data = country_data.shipping_rules.get(country)
+    if data:
+        return ShippingRules(data)
+    else:
+        return -23
