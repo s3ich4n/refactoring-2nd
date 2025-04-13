2b54237bbf6fc7772486374e7b6ae16dcf02a5c3
s3ich4n <s3ich4n@gmail.com>
2025-04-13 21:33:05 +0900

    feat(chapter12): add monthly_cost method to Party class as an abstract method

--- chapter12/src/case01/case01.py
+++ chapter12/src/case01/case01.py
@@ -5,6 +5,9 @@ class Party:
     def annual_cost(self):
         return self.monthly_cost * 12
 
+    def monthly_cost(self):
+        raise NotImplementedError()
+
 
 class Department(Party):
     def __init__(self):
