7ad4f002223adc6ceb0942a34afcf8235f9114de
s3ich4n <s3ich4n@gmail.com>
2025-04-21 22:46:14 +0900

    feat(chapter12): refactor Booking and PremiumBooking classes for delegation and encapsulation

--- chapter12/src/case10/case10_1.py
+++ chapter12/src/case10/case10_1.py
@@ -1,14 +1,8 @@
-from dataclasses import (
-    dataclass,
-    field,
-)
-from typing import (
-    Dict,
-    Any,
-)
+from dataclasses import dataclass, field
+from typing import Dict, Any
 
 
-@dataclass(frozen=True)
+@dataclass
 class Show:
     price: int
     properties: Dict[str, Any] = field(default_factory=dict)
@@ -17,7 +11,7 @@ class Show:
         return property_name in self.properties
 
 
-@dataclass(frozen=True)
+@dataclass
 class Extras:
     premium_fee: int
     properties: Dict[str, Any] = field(default_factory=dict)
@@ -30,6 +24,7 @@ class Booking:
     def __init__(self, show, date):
         self._show = show
         self._date = date
+        self._premium_delegate = None
 
     def is_peakday(self):
         """날짜가 성수기인지 확인"""
@@ -37,8 +32,18 @@ class Booking:
         return self._date.weekday() >= 5  # 주말(토,일)은 성수기
 
     def has_talk_back(self):
-        """비성수기에 관객과의 대화시간이 있나 점검"""
-        return self._show.has_own_property("talkback") and not self.is_peakday()
+        """비성수기에 관객과의 대화시간이 있나 점검
+
+        Notes:
+            처음 __init__에는 관련 변수를 뺀다
+            추후 _be_premium 을 호출하면 위임하는 객체를 추가한다
+
+        """
+        return (
+            self._premium_delegate.has_talk_back()
+            if self._premium_delegate is not None
+            else self._show.has_own_property("talkback") and not self.is_peakday()
+        )
 
     def base_price(self):
         result = self._show.price
@@ -55,10 +60,6 @@ class PremiumBooking(Booking):
         super().__init__(show, date)
         self._extras = extras
 
-    def has_talk_back(self):
-        """(성수기와 무관) 관객과의 대화시간이 있나 점검"""
-        return self._show.has_own_property("talkback")
-
     def base_price(self):
         return round(super().base_price() + self._extras.premium_fee)
 
@@ -66,6 +67,7 @@ class PremiumBooking(Booking):
         return self._extras.has_own_property("dinner") and not self.is_peakday()
 
 
+# 팩토리 함수
 def create_booking(show, date):
     return Booking(show, date)
 
@@ -73,7 +75,7 @@ def create_booking(show, date):
 def create_premium_booking(show, date, extras):
     result = PremiumBooking(show, date, extras)
     result._be_premium(extras)
-    return PremiumBooking(show, date, extras)
+    return result
 
 
 class PremiumBookingDelegate:
@@ -81,3 +83,7 @@ class PremiumBookingDelegate:
         """예약객체로 향하는 역참조(back-reference)를 매개변수로 받음"""
         self._host = host_booking
         self._extras = extras
+
+    def has_talk_back(self):
+        """(성수기와 무관) 관객과의 대화시간이 있나 점검"""
+        return self._host._show.has_own_property("talkback")
