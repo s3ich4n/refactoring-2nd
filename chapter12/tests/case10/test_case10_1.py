de4fa1369db4b2e22a5399516b53cbe5e3b8bde9
s3ich4n <s3ich4n@gmail.com>
2025-04-21 22:04:42 +0900

    feat(chapter12): update tests to use factory functions for Booking and PremiumBooking

--- chapter12/tests/case10/test_case10_1.py
+++ chapter12/tests/case10/test_case10_1.py
@@ -5,8 +5,8 @@ import pytest
 from chapter12.src.case10.case10_1 import (
     Show,
     Extras,
-    Booking,
-    PremiumBooking,
+    create_booking,
+    create_premium_booking,
 )
 
 
@@ -38,11 +38,11 @@ def weekend():
 # 일반 예약 테스트
 def test_regular_booking_base_price(show_with_talkback, weekday, weekend):
     # 주중 예약
-    weekday_booking = Booking(show_with_talkback, weekday)
+    weekday_booking = create_booking(show_with_talkback, weekday)
     assert weekday_booking.base_price() == 100
 
     # 주말(성수기) 예약
-    weekend_booking = Booking(show_with_talkback, weekend)
+    weekend_booking = create_booking(show_with_talkback, weekend)
     assert weekend_booking.base_price() == 115  # 100 + 15% 할증
 
 
@@ -50,15 +50,15 @@ def test_regular_booking_has_talkback(
     show_with_talkback, show_without_talkback, weekday, weekend
 ):
     # 주중 예약, 톡백 있음
-    weekday_booking = Booking(show_with_talkback, weekday)
+    weekday_booking = create_booking(show_with_talkback, weekday)
     assert weekday_booking.has_talk_back() is True
 
     # 주중 예약, 톡백 없음
-    weekday_booking_no_talkback = Booking(show_without_talkback, weekday)
+    weekday_booking_no_talkback = create_booking(show_without_talkback, weekday)
     assert weekday_booking_no_talkback.has_talk_back() is False
 
     # 주말(성수기) 예약, 톡백 있음 - 성수기에는 톡백이 없어야 함
-    weekend_booking = Booking(show_with_talkback, weekend)
+    weekend_booking = create_booking(show_with_talkback, weekend)
     assert weekend_booking.has_talk_back() is False
 
 
@@ -67,11 +67,15 @@ def test_premium_booking_base_price(
     show_with_talkback, weekday, weekend, extras_with_dinner
 ):
     # 주중 프리미엄 예약
-    weekday_premium = PremiumBooking(show_with_talkback, weekday, extras_with_dinner)
+    weekday_premium = create_premium_booking(
+        show_with_talkback, weekday, extras_with_dinner
+    )
     assert weekday_premium.base_price() == 150  # 100 + 50 프리미엄 요금
 
     # 주말(성수기) 프리미엄 예약
-    weekend_premium = PremiumBooking(show_with_talkback, weekend, extras_with_dinner)
+    weekend_premium = create_premium_booking(
+        show_with_talkback, weekend, extras_with_dinner
+    )
     assert weekend_premium.base_price() == 165  # 115 + 50 프리미엄 요금
 
 
@@ -79,27 +83,48 @@ def test_premium_booking_has_talkback(
     show_with_talkback, show_without_talkback, weekday, weekend, extras_with_dinner
 ):
     # 주중 프리미엄 예약, 톡백 있음
-    weekday_premium = PremiumBooking(show_with_talkback, weekday, extras_with_dinner)
+    weekday_premium = create_premium_booking(
+        show_with_talkback,
+        weekday,
+        extras_with_dinner,
+    )
     assert weekday_premium.has_talk_back() is True
 
     # 주말(성수기) 프리미엄 예약, 톡백 있음 - 프리미엄은 성수기와 관계없이 톡백 있음
-    weekend_premium = PremiumBooking(show_with_talkback, weekend, extras_with_dinner)
+    weekend_premium = create_premium_booking(
+        show_with_talkback,
+        weekend,
+        extras_with_dinner,
+    )
     assert weekend_premium.has_talk_back() is True
 
     # 주중 프리미엄 예약, 톡백 없음
-    weekday_premium_no_talkback = PremiumBooking(
-        show_without_talkback, weekday, extras_with_dinner
+    weekday_premium_no_talkback = create_premium_booking(
+        show_without_talkback,
+        weekday,
+        extras_with_dinner,
     )
     assert weekday_premium_no_talkback.has_talk_back() is False
 
 
 def test_premium_booking_has_dinner(
-    show_with_talkback, weekday, weekend, extras_with_dinner
+    show_with_talkback,
+    weekday,
+    weekend,
+    extras_with_dinner,
 ):
     # 주중 프리미엄 예약, 저녁 있음
-    weekday_premium = PremiumBooking(show_with_talkback, weekday, extras_with_dinner)
+    weekday_premium = create_premium_booking(
+        show_with_talkback,
+        weekday,
+        extras_with_dinner,
+    )
     assert weekday_premium.has_dinner() is True
 
     # 주말(성수기) 프리미엄 예약, 저녁 있음 - 성수기에는 저녁이 없어야 함
-    weekend_premium = PremiumBooking(show_with_talkback, weekend, extras_with_dinner)
+    weekend_premium = create_premium_booking(
+        show_with_talkback,
+        weekend,
+        extras_with_dinner,
+    )
     assert weekend_premium.has_dinner() is False
