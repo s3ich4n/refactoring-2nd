632f282577380c32f3d688a7c2b88189dbe70e21
s3ich4n <s3ich4n@gmail.com>
2025-04-22 12:56:01 +0900

    feat(chapter08): implement overdraft_charge method in AccountType and refactor Account class

--- chapter08/src/case01/case01_2.py
+++ chapter08/src/case01/case01_2.py
@@ -1,40 +1,50 @@
-
 class AccountType:
     def __init__(self, is_premium=False):
         self._is_premium = is_premium
-    
+
     @property
     def is_premium(self):
         return self._is_premium
+
+    def overdraft_charge(self, days_overdrawn=0):
+        """
+
+        Notes:
+            일단 값으로 넘기고,
+            overdrawn date 이상의 값이 필요하면 `Account` 를 통째로 던지면 됨
+        """
+        if self.is_premium:
+            base_charge = 10
+            if days_overdrawn <= 7:
+                return base_charge
+            else:
+                return base_charge + (days_overdrawn - 7) * 0.85
+        else:
+            return days_overdrawn * 1.75
 
 
 class Account:
     def __init__(self, account_type=None, days_overdrawn=0):
         self._account_type = account_type if account_type else AccountType()
         self._days_overdrawn = days_overdrawn
-    
+
     @property
     def account_type(self):
         return self._account_type
-    
+
     @property
     def days_overdrawn(self):
         return self._days_overdrawn
-    
+
     @property
     def bank_charge(self):
         result = 4.5
         if self.days_overdrawn > 0:
+            # 인라인하는 방법도 있고,
             result += self.overdraft_charge
         return result
-    
+
     @property
     def overdraft_charge(self):
-        if self.account_type.is_premium:
-            base_charge = 10
-            if self.days_overdrawn <= 7:
-                return base_charge
-            else:
-                return base_charge + (self.days_overdrawn - 7) * 0.85
-        else:
-            return self.days_overdrawn * 1.75
+        """직접 위임해서 쓰는 방법도 있다. 나는 이쪽이 더 마음에든다."""
+        return self._account_type.overdraft_charge(self._days_overdrawn)
