aed419667e5409db336e3c9694801b709dc0b5c3
s3ich4n <s3ich4n@gmail.com>
2025-04-24 12:58:57 +0900

    refactor(chapter08): move discount_rate field to CustomerContract for better encapsulation

--- chapter08/src/case02/case02.py
+++ chapter08/src/case02/case02.py
@@ -8,18 +8,22 @@ def date_today():
 class Customer:
     def __init__(self, name, discount_rate):
         self.name = name
+        self._contract = CustomerContract(date_today())
         self.set_discount_rate(discount_rate)
-        self.contract = CustomerContract(date_today())
 
     @property
     def discount_rate(self):
-        return self._discount_rate
+        return self.contract.discount_rate
+
+    @property
+    def contract(self):
+        return self._contract
 
     def set_discount_rate(self, a_number):
-        self._discount_rate = a_number
+        self.contract._discount_rate = a_number
 
     def become_preferred(self):
-        self._discount_rate += 0.03
+        self.set_discount_rate(self.discount_rate + 0.03)
         # other nice things
 
     def apply_discount(self, amount):
@@ -27,5 +31,18 @@ class Customer:
 
 
 class CustomerContract:
-    def __init__(self, start_date):
-        self.start_date = start_date
+    def __init__(self, start_date, discount_rate=None):
+        self._start_date = start_date
+        self._discount_rate = discount_rate
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @property
+    def discount_rate(self):
+        return self._discount_rate
+
+    @discount_rate.setter
+    def discount_rate(self, a_number):
+        self._discount_rate = a_number
