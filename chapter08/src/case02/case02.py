c14c3982edea6e72d061d20d71d423c44c584cf4
s3ich4n <s3ich4n@gmail.com>
2025-04-24 12:49:28 +0900

    refactor(chapter08): update Customer class to use set_discount_rate method for setting discount rate

--- chapter08/src/case02/case02.py
+++ chapter08/src/case02/case02.py
@@ -8,16 +8,15 @@ def date_today():
 class Customer:
     def __init__(self, name, discount_rate):
         self.name = name
-        self._discount_rate = discount_rate
+        self.set_discount_rate(discount_rate)
         self.contract = CustomerContract(date_today())
 
     @property
     def discount_rate(self):
         return self._discount_rate
 
-    @discount_rate.setter
-    def discount_rate(self, value):
-        self._discount_rate = value
+    def set_discount_rate(self, a_number):
+        self._discount_rate = a_number
 
     def become_preferred(self):
         self._discount_rate += 0.03
