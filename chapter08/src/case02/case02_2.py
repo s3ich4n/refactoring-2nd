d068807e98f36d04709e2337c3ad5fb0dd58162f
s3ich4n <s3ich4n@gmail.com>
2025-04-24 13:16:25 +0900

    refactor(chapter08): encapsulate account number and account type fields in Account class

--- chapter08/src/case02/case02_2.py
+++ chapter08/src/case02/case02_2.py
@@ -1,8 +1,33 @@
 class Account:
     def __init__(self, number, account_type, interest_rate):
-        self.number = number
-        self.account_type = account_type
-        self.interest_rate = interest_rate
+        self._number = number
+        self._account_type = account_type
+        # 코어 동작이 바뀌지 않는 선에서 필드를 옮겨야한다.
+        # 그게 아니면 기능 변경이다. 리팩터링 그 이상의 것이 된다는 말.
+        if interest_rate is not None:
+            self._account_type.interest_rate = interest_rate
+
+    @property
+    def interest_rate(self):
+        return self._account_type.interest_rate
+
+    @interest_rate.setter
+    def interest_rate(self, value):
+        self._account_type.interest_rate = value
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def account_type(self):
+        return self._account_type
+
+
+class AccountType:
+    def __init__(self, name_string, interest_rate=None):
+        self._name = name_string
+        self._interest_rate = interest_rate
 
     @property
     def interest_rate(self):
@@ -11,8 +36,3 @@ class Account:
     @interest_rate.setter
     def interest_rate(self, value):
         self._interest_rate = value
-
-
-class AccountType:
-    def __init__(self, name_string):
-        self._name = name_string
